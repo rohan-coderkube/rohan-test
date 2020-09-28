b9269d0e26d2a8f1cf2b0485b090fe8837282218
rohan-coderkube <rohan.coderkube@gmail.com>
2020-09-28 15:53:13 +0530

    commit second to remove issues1

--- app/src/main/java/com/example/mvpdemo/Login/LoignActivity.kt
+++ app/src/main/java/com/example/mvpdemo/Login/LoignActivity.kt
@@ -10,8 +10,6 @@ class LoignActivity : AppCompatActivity(), MainView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loign)
-
-
     }
 
 
@@ -24,3 +22,5 @@ class LoignActivity : AppCompatActivity(), MainView {
     }
 
 }
+
+
