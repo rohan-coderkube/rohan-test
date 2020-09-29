3120573cf097b07cc974d0e4976c7c1e7a59f474
viral-coderkube <viral.coderkube@gmail.com>
2020-09-29 15:06:57 +0530

    viral side update

--- app/src/main/java/com/example/mvpdemo/Base/BaseActivity.kt
+++ app/src/main/java/com/example/mvpdemo/Base/BaseActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mvpdemo.Base
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -8,9 +9,13 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.e("pika","pika")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.e("pika","pika")
+
     }
 }
